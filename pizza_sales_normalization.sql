0b2164d2412c5b4507e97f64527146e6ad44e7d9
AbdullahSafdarW <abdullahsa.pisjesy91@gmail.com>
2025-08-22 22:26:56 +0500

    created table with order_id PK and total_price

--- pizza_sales_normalization.sql
+++ pizza_sales_normalization.sql
@@ -4,14 +4,20 @@ SELECT * FROM pizza_sales;
 ALTER TABLE pizza_sales
 DROP COLUMN pizza_id;
 
--- -------      Creating Tables to Normalize Database up to 3NF       -------------
+-- -------      Creating Tables to Normalize Relational Database up to 3NF       -------------
+
+-- New table Order Totals
 
 CREATE TABLE order_totals AS
-SELECT 
-    order_id, 
-    SUM(total_price) AS total_price
-FROM pizza_sales
+SELECT order_id, SUM(total_price) AS total_order_price FROM pizza_sales
 GROUP BY order_id;
 
+ALTER TABLE order_totals
+MODIFY order_id INT PRIMARY KEY; -- order_id now a PK
+
 SELECT * FROM order_totals;
-SELECT SUM(total_price) AS Total_Revenue FROM order_totals; -- 817860
+SELECT SUM(total_order_price) AS Total_Revenue FROM order_totals; -- 817860 (testing if revenue matches previous table's query)
+
+-- New table Pizza Flavour
+
+CREATE TABLE Pizza_Flavour
